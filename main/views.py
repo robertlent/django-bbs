ff89a6784085f2db2e7b9d0e5ae243c466696ee8
Robert Lent <robert@therobertlent.com>
2023-04-16 23:31:10 -0700

    Add theming/styling to site

--- main/views.py
+++ main/views.py
@@ -118,18 +118,22 @@ def user_profile(request, primary_key):
 @login_required(login_url='/login')
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
 
     if request.method == 'POST':
-        form = RoomForm(request.POST)
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
 
-            return redirect('home')
+        return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
 
     return render(request, 'main/room_form.html', context)
 
@@ -138,19 +142,23 @@ def create_room(request):
 def update_room(request, primary_key):
     room = Room.objects.get(id=primary_key)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse("You are not authorized to do that!")
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
 
-        if form.is_valid():
-            form.save()
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
 
-            return redirect('home')
+        return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room}
 
     return render(request, 'main/room_form.html', context)
 
@@ -164,6 +172,7 @@ def delete_room(request, primary_key):
 
     if request.method == 'POST':
         room.delete()
+
         return redirect('home')
 
     return render(request, 'main/delete.html', {'obj': room})
